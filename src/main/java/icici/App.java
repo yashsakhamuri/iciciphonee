2a3a68eff75835de788acbdaeb644a3af2bffb16
yashsakhamuri <ys@gm.com>
2021-05-08 07:15:46 +0530

    target

--- src/main/java/icici/App.java
+++ src/main/java/icici/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( hi new code developed );
+        System.out.println( "Hello world!" );
     }
 }
